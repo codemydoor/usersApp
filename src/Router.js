ef146b4120e86148964c25a3ce7ce9a12ca53e8b
Edmund Quashie <dwolino@gmail.com>
2020-08-05 13:01:37 +0000

    fbcrude

--- src/Router.js
+++ src/Router.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import  App from './App';
-import EditForm from './components/EditForm';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import App from "./App";
+import EditForm from "./components/EditForm";
 
 function Router() {
-    return (
-        <BrowserRouter>
-
-         <Route exact path="/"  component={App} />
-         <Route exact path="/edit/:id"  component={EditForm} />
-
-        </BrowserRouter>
-        
-    );
+  return (
+    <BrowserRouter>
+      <Route exact path="/" component={App} />
+      <Route exact path="/edit/:id" component={EditForm} />
+    </BrowserRouter>
+  );
 }
 
 export default Router;
